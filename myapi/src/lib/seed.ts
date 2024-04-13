11e35d3105a59e14a4892b82b886ef143a1322f5
yunqilee <yli380479@gmail.com>
2024-04-12 23:34:38 -0500

    set the configuration of Redis

--- myapi/src/lib/seed.ts
+++ myapi/src/lib/seed.ts
@@ -1,5 +1,18 @@
+import {Redis} from "@upstash/redis";
+import dotenv from 'dotenv';
 import airportsData from './airports.json';
 
 const airports_name = airportsData.airports;
 
-console.log(airports_name.length);
+dotenv.config({path: '.env.local'});
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+})
+
+redis.set('key', 'value').then(result => {
+    console.log(result);
+});
+
+console.log(airports_name.length)
