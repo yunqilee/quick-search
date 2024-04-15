b5fb5278e315971ae7c808027bddb681bea18bf4
yunqilee <yli380479@gmail.com>
2024-04-14 22:40:03 -0500

    Update route.ts

--- myapi/src/app/api/[[...route]]/route.ts
+++ myapi/src/app/api/[[...route]]/route.ts
@@ -1,12 +1,48 @@
 import {Hono} from "hono";
+import {env} from "hono/adapter"
 import {handle} from "hono/vercel";
+import {Redis} from "@upstash/redis/cloudflare";
 
 export const runtime = "edge"
 
 const app = new Hono().basePath('/api')
 
-app.get('/search', (c) => {
-    return c.json({})
+type EnvConfig = {
+    UPSTASH_REDIS_REST_URL: string
+    UPSTASH_REDIS_REST_TOKEN: string
+}
+
+app.get('/search', async (c) => {
+    const {UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN} = env<EnvConfig>(c)
+
+    const redis = new Redis({
+        url: UPSTASH_REDIS_REST_URL,
+        token: UPSTASH_REDIS_REST_TOKEN,
+    })
+
+    const query = c.req.query('q')?.toLowerCase()
+
+    const resp = []
+    const rank = await redis.zrank('2terms', query)
+    if (rank !== null && rank !== undefined) {
+        const temp = await redis.zrange<string[]>('2terms', rank, rank + 100)
+
+        for (const el of temp) {
+            // @ts-ignore
+            if (!el.startsWith(query)) {
+                break
+            }
+
+            if (el.endsWith('*')) {
+                resp.push(el.substring(0, el.length - 1))
+            }
+        }
+    }
+    console.log(resp)
+    return c.json({
+        "response": resp,
+    })
+
 })
 
 export const GET = handle(app)
