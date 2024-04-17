09c64b2dff570ba86b5cdd175dbddc7fbf678bf7
yunqilee <yli380479@gmail.com>
2024-04-16 21:13:58 -0500

    set the page

--- myapi/src/app/page.tsx
+++ myapi/src/app/page.tsx
@@ -21,13 +21,18 @@ export default function Home() {
 
   return (
       <main className="h-screen w-screen grainy">
-        <input
-            value={input}
-            onChange={(e) => {
-              setInput(e.target.value)
-            }}
-            placeholder="Search airports..."
-            className="text-zinc-500" type="text"/>
+          <div className="flex flex-col  gap-6 items-center pt-32 duration-500 animate-in
+          fade-in-5 slide-in-from-bottom-2.5">
+              <h1 className="text-5xl text-zinc-900 tracking-tight font-bold">QuickSearch ✈️</h1>
+              <p className="text-zinc-600 text-lg max-w-prose text-center">A high-performance API combined with Next.js, powered by Upstash Redis</p>
+              <input
+                  value={input}
+                  onChange={(e) => {
+                      setInput(e.target.value)
+                  }}
+                  placeholder="Search airports..."
+                  className="text-zinc-500" type="text"/>
+          </div>
       </main>
   );
 }
